5bdf4bc91017904eef6750bd1caf0080ae593788
Danny <Danny@DESKTOP-2LOI23N.hub>
2022-09-08 13:55:52 +1200

    console based system working

--- CourseCalc/src/Calc.java
+++ CourseCalc/src/Calc.java
@@ -1,46 +1,63 @@
-import java.awt.BorderLayout;
 
-import javax.swing.*;
+import java.math.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 
 /**
  * @author Danny
  * 
- * Grade Calculator so to find out what mark you need on your next assignment.
+ * Grade Calculator to find out what mark you need on your next assignment.
  *
  */
 public class Calc {
+	private Scanner sc = new Scanner(System.in);
+	private ArrayList<Double> grade = new ArrayList<Double>();
+	private ArrayList<Integer> weight = new ArrayList<Integer>();
 	
-	private JTextArea displayMessage;
 	
-	
-	private void jButtionActionPerformed(java.awt.event.ActionEvent evt) {
-		
-	}
-	
-	/**
-	 * Creates the frame and sets up the calculator
-	 * @param args
-	 */
 	public static void main(String args[]) {
 		Calc c = new Calc();
-		c.create();
+		c.askHowMuch();
+		c.showGrade();
+		
+	}
+	private void showGrade() {
+		double total = 0;
+		for(int i = 0; i != grade.size(); ++i) {
+			System.out.println("Your grade for Assignment " + i + " is " + grade.get(i));
+			total = total + grade.get(i);
+		}
+		System.out.println("Your Total mark is " + total + " out of 100");
+		
+	}
+	public void askHowMuch() {
+		System.out.println("How many assignments have you done\n ");
+		int ammount = sc.nextInt();
+		
+		for(int i = 0; i != ammount; ++i) {
+			System.out.println("What was the mark of your assignment?\n ");
+			int a = sc.nextInt();
+			System.out.println("What was the total mark of your assignment?\n ");
+			int b = sc.nextInt();
+			double ran = (double)a / (double)b;
+				
+			System.out.println("What was the weight of your assignment? ");
+			int c = sc.nextInt();
+
+			
+			weight.add(c);			
+			grade.add(ran*(double)c);
+			
+		}
+		
 	}
 	
-	/**
-	 * 
-	 */
-	public void create() {
-		JFrame frame = new JFrame("Main Pannel");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		frame.pack();
-		frame.setVisible(true);
+	public Double doMaths(double a, int b) {
+		double ans = a*b;
+		return ans;
 	}
-	public void doMaths() {
-		
-	}
+	
 }
 
